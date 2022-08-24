3233ebff81563eafa15317b46a07d792fa7b55fa
User <User@DESKTOP-P6508QM>
2022-08-24 15:58:43 +0530

    GitHub

---
+++ src/test/java/github/Hello1.java
@@ -0,0 +1,11 @@
+package github;
+
+public class Hello1 {
+public static void main(String[] args) {
+	System.out.println("hi1");
+	int a=10;
+	int b=30;
+	int c=a+b;
+	System.out.println("c");
+}
+}
